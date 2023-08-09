6e7ce35363b235d006a528167900c89d18f35bce
DevWavez <141139262+DevWavez@users.noreply.github.com>
2023-08-09 17:25:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(() => {
+  var magic8Ball = {};
+  magic8Ball.listOfAnswers = [
+    "No",
+    "Yes",
+    "I don't think so...",
+    "Of course!",
+    "Indubitably",
+    "In your dreams.",
+  ];
+
+  $("#answer").hide();
+
+  magic8Ball.askQuestion = (question) => {
+    $("#8ball").effect("shake");
+
+    $("#8ball").attr(
+      "src",
+      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png"
+    );
+
+    $("#answer").fadeIn(4000);
+
+    var randomNumber = Math.random();
+    var randomNumberForListOfAnswer = randomNumber * this.listOfAnswers.length;
+    var randomIndex = Math.floor(randomNumberForListOfAnswer);
+    var answer = this.listOfAnswers[randomIndex];
+
+    $("#answer").text(answer);
+
+    console.log(question);
+    console.log(answer);
+  };
+
+  var onClick = function () {
+    $("#answer").hide();
+
+    $("#8ball").attr(
+      "src",
+      "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png"
+    );
+
+    setTimeout(function () {
+      var question = prompt("ASK A YES/NO QUESTION!");
+      magic8Ball.askQuestion(question);
+    }, 500);
+  };
+
+  $("#questionButton").click(onClick);
+});
